fe5d3c8f6624a868fe2852512825381bf8c175d5
Alexandra Mashenkova <sasham597@gmail.com>
2014-10-02 23:15:52 +0300

    Add some styling

--- Contacts/gen/com/contacts/R.java
+++ Contacts/gen/com/contacts/R.java
@@ -14,12 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int input_name=0x7f050003;
-        public static final int input_phone_number=0x7f050005;
-        public static final int input_surname=0x7f050004;
-        public static final int name=0x7f050000;
-        public static final int phone=0x7f050002;
-        public static final int surname=0x7f050001;
+        public static final int input_name=0x7f060003;
+        public static final int input_phone_number=0x7f060005;
+        public static final int input_surname=0x7f060004;
+        public static final int name=0x7f060000;
+        public static final int phone=0x7f060002;
+        public static final int surname=0x7f060001;
     }
     public static final class layout {
         public static final int displaying_contacts=0x7f030000;
@@ -33,5 +33,16 @@ public final class R {
         public static final int input_name=0x7f040002;
         public static final int input_phone_number=0x7f040004;
         public static final int input_surname=0x7f040003;
+    }
+    public static final class style {
+        public static final int button=0x7f050003;
+        public static final int editText=0x7f050000;
+        public static final int editText_nameInput=0x7f050001;
+        public static final int editText_phoneInput=0x7f050002;
+        public static final int horizontalLayout=0x7f050008;
+        public static final int layout_props=0x7f050004;
+        public static final int textView=0x7f050005;
+        public static final int textView_name=0x7f050006;
+        public static final int textView_phone=0x7f050007;
     }
 }
